9f50929848493a10fd7949057f2041f8e4324c70
Wizzard <wizzardsk@gmail.com>
2026-06-04 21:16:10 +0200

    Translation update (sk)

---
+++ lang/sk/lang.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Wizzard <wizzardsk@gmail.com>
+ */
+$lang['badpasspolicy']         = 'Musíte použiť silnejšie heslo.';
+$lang['length']                = 'Vaše heslo musí mať aspoň %d znakov.';
+$lang['pools']                 = 'Vaše heslo musí používať znaky aspoň %d z nasledujúcich typov: %s.';
+$lang['user1']                 = 'Vaše heslo nesmie obsahovať vaše používateľské meno.';
+$lang['user2']                 = 'Vaše heslo môže obsahovať najviac %d po sebe idúcich znakov, ktoré sa vyskytujú vo vašom používateľskom mene.';
+$lang['nocommon']              = 'Vaše heslo nesmie byť jedným z 10 000 najpoužívanejších hesiel.';
+$lang['noleaked']              = 'Vaše heslo sa nesmie vyskytovať v žiadnom známom úniku hesiel.';
+$lang['resendpwd']             = 'Ak účet existuje, danému používateľovi bol odoslaný potvrdzovací e-mail.';
+$lang['js']['strength0']       = 'veľmi slabé';
+$lang['js']['strength1']       = 'slabé';
+$lang['js']['strength2']       = 'slušné';
+$lang['js']['strength3']       = 'silné';
+$lang['js']['nopolicy']        = ', nespĺňa politiku';
---
+++ lang/sk/settings.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Wizzard <wizzardsk@gmail.com>
+ */
+$lang['minlen']                = 'Minimálna dĺžka používateľských hesiel';
+$lang['pools']                 = 'Typy znakov používané v heslách';
+$lang['minpools']              = 'Minimálny počet rôznych typov znakov, ktoré musia byť v heslách použité. Nesmie byť vyšší než počet vybraných typov vyššie';
+$lang['user']                  = 'Kontrolovať, či sa heslo zhoduje s menom používateľa. 0 = vypnuté. 1 = presné zhody. Iné číslo = počet po sebe idúcich znakov, ktoré sa môžu vyskytovať v hesle aj používateľskom mene';
+$lang['nocommon']              = 'Kontrolovať heslo voči zoznamu 10 000 najbežnejších hesiel.';
+$lang['noleaked']              = 'Kontrolovať heslo cez passwords API na haveibeenpwned.com (pomocou k-anonymity), aby sa predišlo heslám, ktoré už unikli.';
+$lang['autotype']              = 'Ako generovať heslá?';
+$lang['autobits']              = 'Minimálny počet bitov informácie na generovanie hesiel. Čím vyšší, tým bezpečnejšie, ale ťažšie na zapamätanie. Minimum: 24.';
+$lang['supressuserhints']      = 'Mechanizmus obnovenia hesla zvyčajne prezradí, či daný používateľský účet existuje. Toto všetky takéto náznaky potlačí.';
+$lang['autotype_o_random']     = 'náhodné heslo';
+$lang['autotype_o_pronouncable'] = 'vysloviteľné heslo';
+$lang['autotype_o_phrase']     = 'viacslovná fráza';
+$lang['pools_lower']           = 'malé písmená';
+$lang['pools_upper']           = 'veľké písmená';
+$lang['pools_numeric']         = 'číslice';
+$lang['pools_special']         = 'špeciálne znaky (napr. !, $, #, %)';
